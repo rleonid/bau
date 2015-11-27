42fe8fbcda8ea2b6ea81acb26e439ae53c7d0c20
Leonid Rozenberg <leonidr@gmail.com>
2015-11-27 18:36:48 -0500

    add col sums and script standalone

--- src/scripts/profile.ml
+++ src/scripts/profile.ml
@@ -1,4 +1,4 @@
-open Bigarrayo
+open Bigarray
 
 let time s f =
   let n = Sys.time () in
@@ -16,24 +16,84 @@ let generate ?(u=1e3) n m =
   let fl64c = Array2.of_array Float64 C_layout native in
   native, fl64for, fl64c
 
+let isf (type ll)(l : ll layout) =
+  match l with
+  | Fortran_layout -> true
+  | C_layout       -> false
+
+let to_offset l = if isf l then (fun i -> i + 1) else (fun i -> i)
+
+let foreach l n f =
+  if isf l then
+    for i = 1 to n do f i done
+  else
+    for i = 0 to n - 1 do f i done
+
+module A2 = struct
+  include Array2
+
+  let row_folds a f i =
+    let l = layout a in
+    let n = dim2 a in
+    let v = ref i in
+    let o = to_offset l in
+    Array.init (dim1 a) (fun r ->
+        v := i;
+        foreach l n (fun c -> v := f !v (unsafe_get a (o r) c));
+        !v)
+
+  let col_folds a f i =
+    let l = layout a in
+    let n = dim1 a in
+    let v = ref i in
+    let o = to_offset l in
+    Array.init (dim2 a) (fun c ->
+      v := i;
+      foreach l n (fun r -> v := f !v (unsafe_get a r (o c)));
+      !v)
+
+end
+
 let sum_rows_n = Array.map (Array.fold_left (+.) 0.)
 
-let sum_rows_f m = A2.row_folds m (+.) 0.0
+let sum_rows_b m = A2.row_folds m (+.) 0.0
 
-let sum_rows_c m = A2.row_folds m (+.) 0.0
+let sum_cols_n m =
+  let r = Array.make (Array.length m.(0)) 0.0 in
+  Array.iter (Array.iteri (fun i v -> r.(i) <- r.(i) +. v)) m;
+  r
+
+let sum_cols_b m = A2.col_folds m (+.) 0.0
 
 let test samples n m =
   let data = Array.init samples (fun _ -> generate n m) in
   let tn () = ignore (Array.map (fun (n, _, _) -> ignore (sum_rows_n n)) data) in
-  let tf () = ignore (Array.map (fun (_, f, _) -> ignore (sum_rows_f f)) data) in
-  let tc () = ignore (Array.map (fun (_, _, c) -> ignore (sum_rows_c c)) data) in
-  time "native" tn; 
-  time "fortran" tc; 
-  time "c" tf 
+  let tf () = ignore (Array.map (fun (_, f, _) -> ignore (sum_rows_b f)) data) in
+  let tc () = ignore (Array.map (fun (_, _, c) -> ignore (sum_rows_b c)) data) in
+  time "native" tn;
+  time "fortran" tc;
+  time "c" tf;
+  let tn () = ignore (Array.map (fun (n, _, _) -> ignore (sum_cols_n n)) data) in
+  let tf () = ignore (Array.map (fun (_, f, _) -> ignore (sum_cols_b f)) data) in
+  let tc () = ignore (Array.map (fun (_, _, c) -> ignore (sum_cols_b c)) data) in
+  time "native" tn;
+  time "fortran" tc;
+  time "c" tf
 
-(* ocamlbuild -use-ocamlfind -package bigarray -I src/scripts/ -I src/lib/ bau.cma profile.native *)
 let () =
   if not (!Sys.interactive) then
     test (int_of_string Sys.argv.(1))
          (int_of_string Sys.argv.(2))
          (int_of_string Sys.argv.(3))
+
+(* ocamlbuild -use-ocamlfind -package bigarray -I src/scripts/ -I src/lib/ bau.cma profile.native
+   Sample run:
+$ ./profile.native 10 3000 3000
+native    :0.407063
+fortran   :1.794288
+c         :3.703106
+native    :0.312351
+fortran   :3.426450
+c         :1.829178
+*)
+
