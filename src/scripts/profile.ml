c7a3e57dc9b40e3c3371c80a1ce6611f317cf70a
Leonid Rozenberg <leonidr@gmail.com>
2015-12-21 15:45:34 -0500

    Constrain the (big) arrays to get better performance
    Per Markus Mottl's advise. Constraining the type of the fold functions
    used, allows the compiler to generate faster code.

--- src/scripts/profile.ml
+++ src/scripts/profile.ml
@@ -25,37 +25,71 @@ let to_offset l = if isf l then (fun i -> i + 1) else (fun i -> i)
 let a1_fold_left f i a =
   let r = ref i in
   for i = 1 to Array1.dim a do
-    r := f (Array1.unsafe_get a i) !r 
+    r := f (Array1.unsafe_get a i) !r
   done;
   !r
 
 let a1_fold_left_safe f (i : float) a =
   let r = ref i in
   for i = 1 to Array1.dim a do
-    r := f (Array1.get a i) !r 
+    r := f (Array1.get a i) !r
   done;
   !r
 
 let sum_n = Array.fold_left (+.) 0.
-let sum_f = a1_fold_left (+.) 0.
+let sum_n_cons (a : float array) = Array.fold_left (+.) 0. a
+let sum_f_unsafe = a1_fold_left (+.) 0.
 let sum_f_safe = a1_fold_left_safe (+.) 0.
 
 open Lacaml.D
 
 let sum_l v = Vec.sum v
 
+let a1_fold_left_cons f i (a : vec) =
+  let r = ref i in
+  for i = 1 to Array1.dim a do
+    r := f (Array1.unsafe_get a i) !r
+  done;
+  !r
+
+let sum_f_cons = a1_fold_left_cons (+.) 0.
+
+(* The type must be constrained fully:
+  let a1_fold_left_cons2 (type l) f i (a : (float, float64_elt, l) Array1.t) =
+  doesn't work.  *)
+let a1_fold_left_cons2 f i (a : (float, float64_elt, fortran_layout) Array1.t) =
+  let r = ref i in
+  for i = 1 to Array1.dim a do
+    r := f (Array1.unsafe_get a i) !r
+  done;
+  !r
+
+let sum_f_cons2 = a1_fold_left_cons2 (+.) 0.
+
 let test samples n =
-  let data = Array.init samples (fun _ -> generate n) in
-  let tn () = Array.map (fun (n, _, _) -> sum_n n) data in
-  let tf () = Array.map (fun (_, f, _) -> sum_f f) data in
-  let tf_safe () = Array.map (fun (_, f, _) -> sum_f_safe f) data in
-  let tl () = Array.map (fun (_, f, _) -> sum_l f) data in
-  let nn = time "native" tn in
-  let nf = time "fortran" tf in
-  let nf_safe = time "safe" tf_safe in
-  let nl = time "lacaml" tl in
+  let data         = Array.init samples (fun _ -> generate n) in
+  let tn ()        = Array.map (fun (n, _, _) -> sum_n n) data in
+  let tn_cons ()   = Array.map (fun (n, _, _) -> sum_n_cons n) data in
+  let tf_unsafe () = Array.map (fun (_, f, _) -> sum_f_unsafe f) data in
+  let tf_safe ()   = Array.map (fun (_, f, _) -> sum_f_safe f) data in
+  let tl_lacaml () = Array.map (fun (_, f, _) -> sum_l f) data in
+  let tf_cons ()   = Array.map (fun (_, f, _) -> sum_f_cons f) data in
+  let tf_con2 ()   = Array.map (fun (_, f, _) -> sum_f_cons2 f) data in
+  let native  = time "native" tn in
+  let nat_con = time "nat cons" tn_cons in
+  let unsafe  = time "unsafe" tf_unsafe in
+  let safe    = time "safe" tf_safe in
+  let lacaml  = time "lacaml" tl_lacaml in
+  let constra = time "const" tf_cons in
+  let constr2 = time "const 2" tf_con2 in
   Printf.printf "equal %b\n"
-    (nn = nf && nf = nf_safe && nf = nl)
+    (native = nat_con
+     && nat_con = unsafe
+     && unsafe = safe
+     && safe = lacaml
+     && lacaml = constra
+     && constra = constr2
+    )
 
 let () =
   if not (!Sys.interactive) then begin
