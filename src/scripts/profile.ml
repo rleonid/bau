be924a0c61010a84b6916bfd48d9738f3b12271d
Leonid Rozenberg <leonidr@gmail.com>
2015-12-20 23:42:56 -0500

    A few more interactions with row folding

--- src/scripts/profile.ml
+++ src/scripts/profile.ml
@@ -29,6 +29,18 @@ let foreach l n f =
   else
     for i = 0 to n - 1 do f i done
 
+let fold_l stop f i =
+  let rec loop j a =
+    if j > stop then a
+    else let nac = f a j in loop (j + 1) nac
+  in
+  loop 1 i
+
+let row_folds_f a f (i : float) =
+  let n = Array2.dim2 a in
+  Array.init (Array2.dim1 a) (fun r ->
+      fold_l n (fun ac c -> f ac (Array2.get a (r + 1) c)) i)
+
 module A2 = struct
   include Array2
 
@@ -52,11 +64,16 @@ module A2 = struct
       foreach l n (fun r -> v := f !v (unsafe_get a r (o c)));
       !v)
 
+  let col_folds_f a f (i : float) =
+    col_folds a f i
+
+
 end
 
 let sum_rows_n = Array.map (Array.fold_left (+.) 0.)
 
 let sum_rows_b m = A2.row_folds m (+.) 0.0
+let sum_rows_bf m = row_folds_f m (+.) 0.0
 
 let sum_cols_n m =
   let r = Array.make (Array.length m.(0)) 0.0 in
@@ -72,22 +89,33 @@ let sum_cols_l m =
     let s = A2.slice_right m (i + 1) in
     Vec.sum s)
 
+let eq_arr2 p a1 a2 =
+  Array.fold_left (fun (b, i) v -> b && p a2.(i) v, i + 1) (true, 0) a1
+  |> fst
+
 let test samples n m =
   let data = Array.init samples (fun _ -> generate n m) in
-  let tn () = ignore (Array.map (fun (n, _, _) -> ignore (sum_rows_n n)) data) in
-  let tf () = ignore (Array.map (fun (_, f, _) -> ignore (sum_rows_b f)) data) in
-  let tc () = ignore (Array.map (fun (_, _, c) -> ignore (sum_rows_b c)) data) in
-  time "native" tn;
-  time "fortran" tf;
-  time "c" tc;
-  let tn () = ignore (Array.map (fun (n, _, _) -> ignore (sum_cols_n n)) data) in
-  let tf () = ignore (Array.map (fun (_, f, _) -> ignore (sum_cols_b f)) data) in
-  let tc () = ignore (Array.map (fun (_, _, c) -> ignore (sum_cols_b c)) data) in
-  let tl () = ignore (Array.map (fun (_, f, _) -> ignore (sum_cols_l f)) data) in
-  time "native" tn;
-  time "fortran" tf;
-  time "c" tc;
-  time "lacaml" tl
+  let tn () = Array.map (fun (n, _, _) -> ignore (sum_rows_n n)) data in
+  let tf () = Array.map (fun (_, f, _) -> ignore (sum_rows_b f)) data in
+  let tc () = Array.map (fun (_, _, c) -> ignore (sum_rows_b c)) data in
+  let tff () = Array.map (fun (_, f, _) -> ignore (sum_rows_bf f)) data in
+  let nn = time "native" tn in
+  let nf = time "fortran" tf in
+  let nc = time "c" tc in
+  let nff = time "f float" tff in
+  Printf.printf "equal %b \n" (nn = nf && nf = nc && nc = nff);
+  let tn () = Array.map (fun (n, _, _) -> ignore (sum_cols_n n)) data in
+  let tf () = Array.map (fun (_, f, _) -> ignore (sum_cols_b f)) data in
+  let tc () = Array.map (fun (_, _, c) -> ignore (sum_cols_b c)) data in
+  let tl () = Array.map (fun (_, f, _) -> ignore (sum_cols_l f)) data in
+  let nn = time "native" tn in
+  let nf = time "fortran" tf in
+  let nc = time "c" tc; in
+  let nl = time "lacaml" tl in
+  Printf.printf "equal %b %b %b\n"
+    (eq_arr2 (=) nn nf)
+    (eq_arr2 (=) nf nc)
+    (eq_arr2 (=) nc nl)
 
 let () =
   if not (!Sys.interactive) then begin
