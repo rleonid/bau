1b0b00e63be5cf1eb74375722df8c2f99d7769fa
Leonid Rozenberg <leonidr@gmail.com>
2015-12-27 01:14:50 -0500

    Support for specified and unspecified layouts

--- src/fold_ppx/fold_ppx.ml
+++ src/fold_ppx/fold_ppx.ml
@@ -3,6 +3,8 @@ open Parsetree
 open Ast_mapper
 open Ast_helper
 
+open Bigarray
+
 (* Utilities *)
 let split c s =
   let rec loop o =
@@ -14,6 +16,25 @@ let split c s =
   in
   loop 0
 
+(* Bigarray specific transforms *)
+
+let kind_to_types = function
+  | Float64 -> "float", "float64_elt"
+  | _ -> failwith "NI"
+
+type bigarray_layout =
+  | Fortran_layout
+  | C_layout
+
+let parse_layout_str = function
+  | "fortran" -> Some Fortran_layout
+  | "c"       -> Some C_layout
+  | _         -> None
+
+let layout_to_fold_parameters = function
+  | Fortran_layout -> "fortran_layout", 1, false
+  | C_layout       -> "c_layout", 0, true
+
 (* AST construction helpers *)
 let to_str ?(loc=Location.none) s = Location.mkloc s loc
 
@@ -23,17 +44,27 @@ let lid ?(loc=Location.none) s =
 let ex_id ?(loc=Location.none) s =
   Exp.ident (lid ~loc s)
 
-let make_fold_left var exp1 exp2 =
-  Exp.let_ Nonrecursive
-    [ Vb.mk (Pat.var (to_str "fold_left"))
-        (Exp.fun_ "" None (Pat.var (to_str "a")) (exp1 ()))]
-    (exp2 ())
+let constrain_vec kind layout_s vec_var =
+  let t1, t2 = kind_to_types kind in
+  let econstr s = Typ.constr (lid s) [] in
+  Pat.constraint_ (Pat.var (to_str vec_var))
+    (Typ.constr (lid "Array1.t")
+       [ econstr t1; econstr t2; econstr layout_s])
+
+let make_fold_left kind layout_opt fold_var vec_var exp1 exp2 =
+  let to_body ls =  Exp.fun_ "" None (constrain_vec kind ls vec_var) exp1 in
+  let body =
+    match layout_opt with
+    | None    -> Exp.newtype "l" (to_body "l")
+    | Some ls -> to_body ls
+  in
+  Exp.let_ Nonrecursive [ Vb.mk (Pat.var (to_str fold_var)) body] exp2
 
 let make_ref var init exp =
   Exp.let_ Nonrecursive
     [ Vb.mk (Pat.var (to_str var))
         (Exp.apply (ex_id "ref") ["", init])]
-    (exp ())
+    exp
 
 let lookup_ref var =
   Exp.apply (ex_id "!") ["", (ex_id var)]
@@ -45,51 +76,87 @@ let assign_ref var val_exp =
 let get_array1 arr index =
   Exp.apply (ex_id "Array1.unsafe_get") [("", (ex_id arr)); ("", (ex_id index))]
 
-let apply_fold fold_f var arr index =
+let apply_f fold_f var arr index =
   Exp.apply fold_f [("", lookup_ref var); ("", get_array1 arr index)]
 
 let make_for_loop index start_exp end_exp body_exp =
   Exp.for_ (Pat.var (to_str index)) start_exp end_exp Upto body_exp
 
-let length_expr arr =
-  Exp.apply (ex_id "Array1.dim") ["",(ex_id arr)]
+let length_expr ~minus_one arr =
+  if minus_one then
+    Exp.apply (ex_id "-")
+      [ "", (Exp.apply (ex_id "Array1.dim") ["",(ex_id arr)])
+      ; "", (Exp.constant (Const_int 1))]
+  else
+    Exp.apply (ex_id "Array1.dim") ["",(ex_id arr)]
 
-let create_fold = function
+(* Create a fast fold using a reference and for-loop. *)
+let create_fold kind layout (fun_exp, init, v) =
+  let open Ast_helper in
+  match layout with
+  | Some l ->
+    let layouts, s, minus_one = layout_to_fold_parameters l in
+    make_fold_left kind (Some layouts) "fold_left" "a"
+      (make_ref "r" init
+        (Exp.sequence
+          (make_for_loop "i"
+            (Exp.constant (Const_int s))
+            (length_expr ~minus_one "a")
+            (assign_ref "r" (apply_f fun_exp "r" "a" "i")))
+          (lookup_ref "r")))
+      (Exp.apply (ex_id "fold_left") ["", v])
+  | None  ->  (* Create a layout agnostic function. *)
+    make_fold_left kind None "fold_left" "b"
+      (let layouts, s, minus_one = layout_to_fold_parameters Fortran_layout in
+      make_fold_left kind (Some layouts) "fold_left_fortran" "a"
+        (make_ref "r" init (
+          Exp.sequence
+            (make_for_loop "i"
+              (Exp.constant (Const_int s))
+              (length_expr ~minus_one "a")
+              (assign_ref "r" (apply_f fun_exp "r" "a" "i")))
+            (lookup_ref "r")))
+        (let layouts, s, minus_one = layout_to_fold_parameters C_layout in
+          make_fold_left kind (Some layouts) "fold_left_c" "a" (
+          make_ref "r" init (
+            Exp.sequence
+              (make_for_loop "i"
+                (Exp.constant (Const_int s))
+                (length_expr ~minus_one "a")
+                (assign_ref "r" (apply_f fun_exp "r" "a" "i")))
+              (lookup_ref "r")))
+            (Exp.match_ (Exp.apply (ex_id "Array1.layout") ["", (ex_id "b")])
+              [ Exp.case (Pat.construct (lid "Fortran_layout") None)
+                  (Exp.apply (ex_id "fold_left_fortran") ["", (ex_id "b")])
+              ; Exp.case (Pat.construct (lid "C_layout") None)
+                  (Exp.apply (ex_id "fold_left_c") ["", (ex_id "b")])])))
+      (Exp.apply (ex_id "fold_left") ["", v])
+
+let parse_payload = function
   | [{ pstr_desc = Pstr_eval
         ({pexp_desc = Pexp_apply
               (fun_exp, [("", init); ("", v)]); _}, _); _ }] ->
-      let open Ast_helper in
-      let fold =
-        make_fold_left "a" (fun () ->
-          make_ref "r" init (fun () -> 
-            Exp.sequence 
-              (make_for_loop "i"
-                (Exp.constant (Const_int 1))
-                (length_expr "a")
-                (assign_ref "r" (apply_fold fun_exp "r" "a" "i"))
-              )
-              (lookup_ref "r")
-              ))
-          (fun () -> 
-            Exp.apply (ex_id "fold_left") ["", v])
-      in
-      fold
-      (*Exp.apply fun_exp [(li,init); (lv, v)] *)
-  | _ ->
-    Ast_helper.Exp.constant (Const_float "(-1.0)")
+    Some (fun_exp, init, v)
+  | _ -> None
 
+let parse txt payload fail =
+  match parse_payload payload with
+  | None -> fail ()
+  | Some t ->
+    match split '.' txt with
+    | ["array1"; "fold_left"; "float64"] -> create_fold Float64 None t
+    | ["array1"; "fold_left"; "float64"; ls] ->
+      match parse_layout_str ls with
+      | Some l -> create_fold Float64 None t
+      | None -> fail () (* TODO: change to explicit failure *)
+    | _ -> fail ()
 
 let bigarray_fold_mapper argv =
   { default_mapper with
     expr = fun mapper expr ->
       match expr with
       | { pexp_desc = Pexp_extension ({ txt }, PStr payload)} ->
-          begin
-            match split '.' txt with
-            | ["array1"; "fold_left"; "float64"] ->
-              create_fold payload
-            | _ -> default_mapper.expr mapper expr 
-          end
+          parse txt payload (fun () -> default_mapper.expr mapper expr)
       | other -> default_mapper.expr mapper other; }
 
 let () =
--- src/fold_ppx/fold_ppx_test.ml
+++ src/fold_ppx/fold_ppx_test.ml
@@ -3,7 +3,7 @@ open Bigarray
 let time s f =
   let n = Sys.time () in
   let r = f () in
-  Printf.printf "%-25s:%f\n" s (Sys.time () -. n);
+  Printf.printf "%-30s:%f\n" s (Sys.time () -. n);
   r
 
 let generate ?(u=1e3) n =
@@ -17,16 +17,10 @@ let generate ?(u=1e3) n =
 
 let sum_n (v : float array) = Array.fold_left (+.) 0. v
 let sum_b v = [%array1.fold_left.float64 (+.) 0. v]
+let sum_b_f v = [%array1.fold_left.float64.fortran (+.) 0. v]
+let sum_b_c v = [%array1.fold_left.float64.c (+.) 0. v]
 
 let () =
-  (*
-  let v = Array1.of_array Float64 Fortran_layout [|1.;2.;3.|] in
-  let add2 y x = x +. y in
-  let four = 4. in
-  if !Sys.interactive then () else
-    Printf.printf "%f\n" [%test];
-    Printf.printf "%f\n" [%array1.fold_left.float64 add2 four v];
-*)
   let samples, n =
     if Array.length Sys.argv < 3 then
       10000, 40
@@ -37,7 +31,13 @@ let () =
   Printf.printf "%d samples of %d \n" samples n;
   let data = Array.init samples (fun _ -> generate n) in
   let test name op = time name (fun () -> Array.map op data) in
-  let native = test "native" (fun (n,_,_) -> sum_n n) in
-  let bgfolf = test "array1 fold_left fortran" (fun (_,f,_) -> sum_b f) in
-  (*let bgfolc = test "array1 fold c layout" (fun (_,_,c) -> sum_b c) in *)
-  Printf.printf "equal %b\n" (native = bgfolf (*&& bgfolf = bgfolc*))
+  let native  = test "native" (fun (n,_,_) -> sum_n n) in
+  let bgfolf  = test "array1 fold_left fortran" (fun (_,f,_) -> sum_b f) in
+  let bgfolf2 = test "array1 fold_left fortran spec" (fun (_,f,_) -> sum_b_f f) in
+  let bgfolc  = test "array1 fold c layout" (fun (_,_,c) -> sum_b c) in
+  let bgfolc2 = test "array1 fold c layout spec" (fun (_,_,c) -> sum_b_c c) in
+  Printf.printf "equal %b\n"
+    (native = bgfolf
+    && bgfolf = bgfolf2
+    && bgfolf2 = bgfolc
+    && bgfolc = bgfolc2)
