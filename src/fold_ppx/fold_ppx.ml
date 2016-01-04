54379f0ab417b301438afe9f3866311c2beae002
Leonid Rozenberg <leonidr@gmail.com>
2016-01-04 14:25:48 -0500

    Wrap original kind and layout types.
    Move failure of parsing code to specific function removing the nesting
    of options.

--- src/fold_ppx/fold_ppx.ml
+++ src/fold_ppx/fold_ppx.ml
@@ -23,66 +23,49 @@ let location_error ?(sub=[]) ?(if_highlight="") ~loc fmt =
     fmt
 
 (* Bigarray specific transforms *)
-(* Is there a better way to handle the GADT's in Bigarray that
-   represent this as opposed to redefining them? *)
-type kinds =
-  | Float32
-  | Float64
-  | Complex32
-  | Complex64
-  | Int8_signed
-  | Int8_unsigned
-  | Int16_signed
-  | Int16_unsigned
-  | Int32
-  | Int64
-  | Int
-  | Nativeint
-  | Char
+type k = K : ('a, 'b) kind -> k
 
-type bigarray_layout =
-  | Fortran_layout
-  | C_layout
-
-let parse_kind = function
-  | "float32"         -> Some Float32
-  | "float64"         -> Some Float64
-  | "complex32"       -> Some Complex32
-  | "complex64"       -> Some Complex64
-  | "int8_signed"     -> Some Int8_signed
-  | "int8_unsigned"   -> Some Int8_unsigned
-  | "int16_signed"    -> Some Int16_signed
-  | "int16_unsigned"  -> Some Int16_unsigned
-  | "int32"           -> Some Int32
-  | "int64"           -> Some Int64
-  | "int"             -> Some Int
-  | "nativeint"       -> Some Nativeint
-  | "char"            -> Some Char
-  | _                 -> None
+let parse_kind ~loc = function
+  | "float32"         -> K Float32
+  | "float64"         -> K Float64
+  | "complex32"       -> K Complex32
+  | "complex64"       -> K Complex64
+  | "int8_signed"     -> K Int8_signed
+  | "int8_unsigned"   -> K Int8_unsigned
+  | "int16_signed"    -> K Int16_signed
+  | "int16_unsigned"  -> K Int16_unsigned
+  | "int32"           -> K Int32
+  | "int64"           -> K Int64
+  | "int"             -> K Int
+  | "nativeint"       -> K Nativeint
+  | "char"            -> K Char
+  | ks                -> location_error ~loc "Unrecognized kind: %s" ks
 
 let kind_to_types = function
-  | Float32         -> "float", "float32_elt"
-  | Float64         -> "float", "float64_elt"
-  | Int8_signed     -> "int", "int8_signed_elt"
-  | Int8_unsigned   -> "int", "int8_unsigned_elt"
-  | Int16_signed    -> "int", "int16_signed_elt"
-  | Int16_unsigned  -> "int", "int16_unsigned_elt"
-  | Int32           -> "int32", "int32_elt"
-  | Int64           -> "int64", "int64_elt"
-  | Int             -> "int", "int_elt"
-  | Nativeint       -> "nativeint", "nativeint_elt"
-  | Complex32       -> "Complex.t","complex32_elt"
-  | Complex64       -> "Complex.t","complex64_elt"
-  | Char            -> "char", "int8_unsigned_elt"
+  | K Float32         -> "float", "float32_elt"
+  | K Float64         -> "float", "float64_elt"
+  | K Int8_signed     -> "int", "int8_signed_elt"
+  | K Int8_unsigned   -> "int", "int8_unsigned_elt"
+  | K Int16_signed    -> "int", "int16_signed_elt"
+  | K Int16_unsigned  -> "int", "int16_unsigned_elt"
+  | K Int32           -> "int32", "int32_elt"
+  | K Int64           -> "int64", "int64_elt"
+  | K Int             -> "int", "int_elt"
+  | K Nativeint       -> "nativeint", "nativeint_elt"
+  | K Complex32       -> "Complex.t","complex32_elt"
+  | K Complex64       -> "Complex.t","complex64_elt"
+  | K Char            -> "char", "int8_unsigned_elt"
 
-let parse_layout_str = function
-  | "fortran" -> Some Fortran_layout
-  | "c"       -> Some C_layout
-  | _         -> None
+type l = L : 'a layout -> l
 
-let layout_to_fold_parameters = function
-  | Fortran_layout -> "fortran_layout", 1, false
-  | C_layout       -> "c_layout", 0, true
+let parse_layout_str ~loc = function
+  | "fortran" -> L Fortran_layout
+  | "c"       -> L C_layout
+  | ls        -> location_error ~loc "Unrecognized layout: %s" ls
+
+let to_fold_params = function
+  | L Fortran_layout -> "fortran_layout", 1, false
+  | L C_layout       -> "c_layout", 0, true
 
 (* AST construction helpers *)
 let to_str ?(loc=Location.none) s = Location.mkloc s loc
@@ -174,37 +157,29 @@ let iter_body ?(vec_arg="a") ~upto ~start ~minus_one fun_exp =
     upto
     (apply_f fun_exp [get_array1 vec_arg "i"])
 
+let operation_to_name_n_body fun_exp init = function
+  | Iter       -> "iter",       (iter_body ~upto:true fun_exp)
+  | Fold true  -> "fold_left",  (fold_body ~upto:true fun_exp init)
+  | Fold false -> "fold_right", (fold_body ~upto:false fun_exp init)
+
 (* Create a fast iter/fold using a reference and for-loop. *)
 let create_layout_specific (f, init, v) op kind layout =
   let open Ast_helper in
-  let layout, start, minus_one = layout_to_fold_parameters layout in
-    let name, body  =
-    match op with
-    | Iter       -> "iter",       iter_body ~upto:true  ~start ~minus_one f
-    | Fold true  -> "fold_left",  fold_body ~upto:true  ~start ~minus_one f init
-    | Fold false -> "fold_right", fold_body ~upto:false ~start ~minus_one f init
-  in
-  make_let ~layout kind name body (Exp.apply (ex_id name) ["", v])
+  let layout, start, minus_one = to_fold_params layout in
+  let name, body = operation_to_name_n_body f init op in
+  make_let ~layout kind name (body ~start ~minus_one) (Exp.apply (ex_id name) ["", v])
 
 (* Create a layout agnostic fold/iter function. *)
 let create (fun_exp, init, v) op kind =
-  let name, name_f, name_c, to_body =
-    match op with
-    | Iter ->
-      "iter", "iter_fortran", "iter_c"
-      , (iter_body ~upto:true fun_exp)
-    | Fold true  ->
-      "fold_left", "fold_left_fortran", "fold_left_c"
-      , (fold_body ~upto:true fun_exp init)
-    | Fold false -> "fold_right", "fold_right_fortran", "fold_right_c"
-      , (fold_body ~upto:false fun_exp init)
-  in
+  let name, body = operation_to_name_n_body fun_exp init op in
+  let name_f = name ^ "_fortran" in
+  let name_c = name ^ "_c" in
   make_let ~arg:"b" kind name
-    (let layout, start, minus_one = layout_to_fold_parameters Fortran_layout in
-    make_let ~layout kind name_f (to_body ~start ~minus_one)
+    (let layout, start, minus_one = to_fold_params (L Fortran_layout) in
+    make_let ~layout kind name_f (body ~start ~minus_one)
       (* intended variable masking *)
-      (let layout, start, minus_one = layout_to_fold_parameters C_layout in
-      make_let ~layout kind name_c (to_body ~start ~minus_one)
+      (let layout, start, minus_one = to_fold_params (L C_layout) in
+      make_let ~layout kind name_c (body ~start ~minus_one)
         (Exp.match_ (Exp.apply (ex_id "Array1.layout") ["", (ex_id "b")])
           [ Exp.case (Pat.construct (lid "Fortran_layout") None)
               (Exp.apply (ex_id name_f) ["", (ex_id "b")])
@@ -226,34 +201,28 @@ let parse_payload loc ba_type f = function
       location_error ~loc "Missing init and %s argument to %s" ba_type f
   | _ -> location_error ~loc "Missing %s arguments" f
 
-let parse_operation = function
-  | "iter"       -> Some Iter
-  | "fold_left"  -> Some (Fold true)
-  | "fold_right" -> Some (Fold false)
-  | _            -> None
+let parse_operation ~loc = function
+  | "iter"       -> Iter
+  | "fold_left"  -> Fold true
+  | "fold_right" -> Fold false
+  | operation    -> location_error ~loc "Unrecognized command: %s" operation
 
-let parse loc operation ks ls_opt payload =
-  match parse_operation operation with
-  | None -> location_error ~loc "Unrecognized command: %s" operation
-  | Some op ->
-    match parse_kind ks with
-    | None -> location_error ~loc "Unrecognized kind: %s" ks
-    | Some kind ->
-      match ls_opt with
-      | None ->
-        let t = parse_payload loc "array1" operation payload in
-        create t op kind
-      | Some ls ->
-        match parse_layout_str ls with
-        | None -> location_error ~loc "Unrecognized layout: %s" ls
-        | Some layout ->
-          let t = parse_payload loc "array1" operation payload in
-          create_layout_specific t op kind layout
+let parse ?layout loc operation ks payload =
+  let op   = parse_operation ~loc operation in
+  let kind = parse_kind ~loc ks in
+  match layout with
+  | None ->
+    let t = parse_payload loc "array1" operation payload in
+    create t op kind
+  | Some ls ->
+    let layout = parse_layout_str ~loc ls in
+    let t = parse_payload loc "array1" operation payload in
+    create_layout_specific t op kind layout
 
 let transform loc txt payload def =
   match split '.' txt with
-  | ["array1"; oper; kind]     -> parse loc oper kind None payload
-  | ["array1"; oper; kind; ls] -> parse loc oper kind (Some ls) payload
+  | ["array1"; oper; kind]         -> parse loc oper kind payload
+  | ["array1"; oper; kind; layout] -> parse ~layout loc oper kind payload
   | _ -> def ()
 
 let bigarray_fold_mapper argv =
