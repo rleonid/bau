5af0811c3db032791115133e19ce84855a6362aa
Leonid Rozenberg <leonidr@gmail.com>
2016-01-06 12:38:28 -0500

    Add Apache copyright notice

--- src/lib/bigarrayo.ml
+++ src/lib/bigarrayo.ml
@@ -1,3 +1,18 @@
+(* Copyright {2016} {Leonid Rozenberg}
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*)
+
 include Bigarray
 
 let isf (type ll)(l : ll layout) =
@@ -180,15 +195,3 @@ module GA = struct
         let full idx = Array.blit idx 0 d 0 nm1; d in
         init (kind ga) l nd (fun idx -> get ga (full idx))
   end
-
-(** Iter/Fold
-  TODO:
-     - Decide which functions to keep (probably not num_elements, apply)
-     - Provide index checking
-     - Write the Array1, Array2, Array3 specialized forms.
-external num_elements : ('a, 'b, 'c) Genarray.t -> int = "num_elements"
-external apply : ('a -> unit) -> ('a, 'b, 'c) Genarray.t -> int array -> unit = "apply"
-external iter : ('a -> unit) -> ('a, 'b, 'c) Genarray.t -> unit = "iter"
-external fold : ('d -> 'a -> 'd) -> 'd -> ('a, 'b, 'c) Genarray.t -> 'd = "fold"
-*)
-(*external iter_slice : ('a, 'b, 'c) Genarray.t -> ('a -> unit) -> int array -> unit = "iter_slice" *)
